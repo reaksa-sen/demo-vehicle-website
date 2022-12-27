38b5dc81293e7b277810240e7896efdb090dde07
Reaksaa <104069671+Reaksaa@users.noreply.github.com>
2022-12-27 22:09:29 +0700

    fix: slider, error 404, about and contact

--- frontend/pages/about/index.tsx
+++ frontend/pages/about/index.tsx
@@ -1,13 +1,12 @@
-import { ContactUs } from "components/About/Contact-us/Contaxt_us";
-import { Description } from "components/About/Description/Description";
-import { Question } from "components/About/Question/Question";
-import { TeamCard } from "components/About/TeamCard/TeamCard";
-import { NextPage } from "next";
-
+import { ContactUs } from 'components/About/Contact-us/Contaxt_us';
+import { Description } from 'components/About/Description/Description';
+import { Question } from 'components/About/Question/Question';
+import { TeamCard } from 'components/About/TeamCard/TeamCard';
+import { NextPage } from 'next';
 
 const About: NextPage = () => {
   return (
-    <div className="container">
+    <div className="container mt-24 px-8">
       <Description />
       <ContactUs />
       <TeamCard />
