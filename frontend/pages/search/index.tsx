d63f326e7390afa88699285a7d25ad2b027d9c7e
Reaksaa <104069671+Reaksaa@users.noreply.github.com>
2023-01-02 16:09:28 +0700

    fix: rename search

--- frontend/pages/search/index.tsx
+++ frontend/pages/search/index.tsx
@@ -1,6 +1,5 @@
-import { From } from 'components/Listings/Form/Form';
-import { ResultCard } from 'components/Listings/ResultCard/ResultCard';
-import { FeatureCar } from 'components/Listings/ResultCard/FeatureCar/FeatureCar';
+import { From } from 'components/Search/Form/Form';
+import { ResultCard } from 'components/Search/ResultCard/ResultCard';
 import { NextPage } from 'next';
 
 const ListingsPage: NextPage = () => {
